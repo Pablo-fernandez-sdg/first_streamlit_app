8bd24b0b9737622ea23c1bb785760dfaec47d325
Pablo-fernandez-sdg <148544951+Pablo-fernandez-sdg@users.noreply.github.com>
2023-11-16 12:19:23 +0100

    Update steamlit_app.py
    Added header
    added text lines

--- steamlit_app.py
+++ steamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('My Parents New Healthy Diner')
+streamlit.text('Omega 3 & Blueberry')
+streamlit.text('Kale, spinach & Rocket smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
